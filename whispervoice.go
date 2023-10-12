e90fba692ec47147b3bd6dc66d51d7974939eb4b
Your Name <you@example.com>
2023-10-12 18:38:52 +0000

    fix

--- whispervoice.go
+++ whispervoice.go
@@ -14,9 +14,20 @@ import (
 )
 
 type TextRep struct {
-	Language string          `json:"language"`
-	Segments [][]interface{} `json:"segments"`
-	Text     string          `json:"text"`
+	Text     string `json:"text"`
+	Segments []struct {
+		ID               int     `json:"id"`
+		Seek             int     `json:"seek"`
+		Start            float64 `json:"start"`
+		End              float64 `json:"end"`
+		Text             string  `json:"text"`
+		Tokens           []int   `json:"tokens"`
+		Temperature      float64 `json:"temperature"`
+		AvgLogprob       float64 `json:"avg_logprob"`
+		CompressionRatio float64 `json:"compression_ratio"`
+		NoSpeechProb     float64 `json:"no_speech_prob"`
+	} `json:"segments"`
+	Language string `json:"language"`
 }
 
 func Voice2Text(file string,url string) (string, error) {
